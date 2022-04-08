39f377907a1ccacedb3d30d8866b5f2e3a3eec2f
八木暁史 <>
2022-04-09 07:59:22 +0900

    手順７　－プレイヤーキャラの移動アニメの同期－

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,15 @@ public class PlayerController : MonoBehaviour
     private float horizontal;                    // x 軸(水平・横)方向の入力の値の代入用
     private float vertical;                      // y 軸(垂直・縦)方向の入力の値の代入用
 
+    private Animator anim;　　　　　　　　　　　 // コンポーネントの取得用
+    private Vector2 lookDirection = new Vector2(0, -1.0f);   // キャラの向きの情報の設定用
 
     void Start()
     {
 
         // このスクリプトがアタッチされているゲームオブジェクトにアタッチされているコンポーネントの中から、<指定>したコンポーネントの情報を取得して、左辺に用意した変数に代入
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,6 +33,9 @@ public class PlayerController : MonoBehaviour
 
         // InputManager の Vertical に登録してあるキーが入力されたら、水平(横)方向の入力値として代入
         vertical = Input.GetAxis("Vertical");
+
+        // キャラの向いている方向と移動アニメの同期
+        SyncMoveAnimation();
     }
 
     void FixedUpdate()
@@ -50,5 +56,32 @@ public class PlayerController : MonoBehaviour
 
         // velocity(速度)に新しい値を代入して、ゲームオブジェクトを移動させる
         rb.velocity = dir * moveSpeed;
+    }
+
+    /// <summary>
+    /// キャラの向いている方向と移動アニメの同期
+    /// </summary>
+    private void SyncMoveAnimation()
+    {
+        // 移動のキー入力値を代入
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        // いずれかのキー入力があるか確認
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            // 向いている方向を更新
+            lookDirection.Set(move.x, move.y);
+
+            // 正規化
+            lookDirection.Normalize();
+
+            // キー入力の値と Blend Tree で設定した移動アニメ用の値を確認し、移動アニメを再生
+            anim.SetFloat("Look X", lookDirection.x);
+            anim.SetFloat("Look Y", lookDirection.y);
+        }
+
+        // 停止アニメーション用
+        anim.SetFloat("Speed", move.magnitude);
+
     }
 }
