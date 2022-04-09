f801e11f67f6f464b104a4b75bed63aea2de9763
八木暁史 <>
2022-04-10 01:34:49 +0900

    手順１０　ーNPC との会話イベントの実装ー

---
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayerCharacter : MonoBehaviour
+{
+    [Header("会話イベント判定用")]
+    public bool isTalking;         // true の場合は会話イベント中であるように扱う
+
+    /// <summary>
+    /// 会話開始
+    /// </summary>
+
+    public void PlayTalk(Vector3 playerPos)
+    {
+        // 会話イベントを行っている状態にする
+        isTalking = true;
+
+        // TODO プレイヤーの位置を確認してウインドウを出す位置を決定する
+
+
+        // TODO 会話イベントのウインドウを表示する
+
+        Debug.Log("会話ウインドウを開く");
+
+    }
+
+    /// <summary>
+    /// 会話終了
+    /// </summary>
+    public void StopTalk()
+    {
+        // 会話イベントをしていない状態にする
+        isTalking = false;
+
+        // TODO 会話イベントのウインドウを閉じる
+
+        Debug.Log("会話ウインドウを閉じる");
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,8 @@ public class PlayerController : MonoBehaviour
     private Animator anim;　　　　　　　　　　　 // コンポーネントの取得用
     private Vector2 lookDirection = new Vector2(0, -1.0f);   // キャラの向きの情報の設定用
 
+    private bool isTalking;                 // 会話イベント中かどうかの判定用。true の場合には会話イベント中
+
     void Start()
     {
 
@@ -27,6 +29,16 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // アクション
+        Action();
+
+        // 会話中は移動のキー入力受付を行わない
+        if (isTalking)
+        {
+            return;
+        }
+
+
 
         // InputManager の Horizontal に登録してあるキーが入力されたら、水平(横)方向の入力値として代入
         horizontal = Input.GetAxis("Horizontal");
@@ -83,5 +95,50 @@ public class PlayerController : MonoBehaviour
         // 停止アニメーション用
         anim.SetFloat("Speed", move.magnitude);
 
+    }
+
+    /// <summary>
+    /// 行動ボタンを押した際の処理
+    /// </summary>
+
+    private void Action()
+    {
+        if (Input.GetButtonDown("Action"))
+        {
+            // Player の位置を起点とし、Player の向いている方向に 1.0f 分だけ Ray を発射し、
+            // NPC レイヤーを持つゲームオブジェクトに接触するか判定し、その情報を hit 変数に代入
+            RaycastHit2D hit = Physics2D.Raycast(rb.position, lookDirection, 1.0f, LayerMask.GetMask("NPC"));
+
+            // Scene ビューにて Ray の可視化
+            Debug.DrawRay(rb.position, lookDirection, Color.red, 1.0f);
+
+            // Ray によって hit 変数にコライダーを持つゲームオブジェクトの情報が取得出来ている（nullではない）場合
+            if (hit.collider != null)
+            {
+                // そのゲームオブジェクトにアタッチされている NonPlayerCharacter クラスが取得できた場合
+                if (hit.collider.TryGetComponent(out NonPlayerCharacter npc))
+                {
+                    // 取得した NonPlayerCharacter クラスを持つゲームオブジェクトと会話中ではない場合
+                    if (!npc.isTalking)
+                    {
+                        // NPC との会話イベントを発生させる
+                        npc.PlayTalk(transform.position);
+
+                        // Player を会話イベント中の状態にする
+                        isTalking = true;
+                    }
+                    // 取得した NonPlayerCharacter クラスを持つゲームオブジェクトと会話中である場合
+                    else
+                    {
+                        // NPC との会話イベントを終了する
+                        npc.StopTalk();
+
+                        // Player を会話イベントをしていない状態にする
+                        isTalking = false;
+                    }
+
+                }
+            }
+        }
     }
 }
