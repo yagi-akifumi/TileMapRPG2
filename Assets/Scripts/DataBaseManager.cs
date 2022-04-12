e6ac601f428dd265ccdef0c87258ad88d62ff096
八木暁史 <>
2022-04-12 21:55:24 +0900

    手順２０　－マスターデータの抽出方法－

--- Assets/Scripts/DataBaseManager.cs
+++ Assets/Scripts/DataBaseManager.cs
@@ -1,13 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class DataBaseManager : MonoBehaviour
 {
     public static DataBaseManager instance;
 
     [SerializeField]
-    private EventDataSO eventDataSO; //スクリプタブル・オブジェクトを登録して管理するための変数を EventDataSO 型で宣言
+    private EventDataSO eventDataSO;    //スクリプタブル・オブジェクトを登録して管理するための変数を EventDataSO 型で宣言
+
+    [SerializeField]
+    private ItemDataSO itemDataSO;      //スクリプタブル・オブジェクトを登録して管理するための変数を ItemDataSO 型で宣言
+
+    // ItemType ごとに分類した List
+    [SerializeField]
+    private List<ItemDataSO.ItemData> BukiItemDatasList = new List<ItemDataSO.ItemData>();
+    [SerializeField]
+    private List<ItemDataSO.ItemData> BouguItemDatasList = new List<ItemDataSO.ItemData>();
+    [SerializeField]
+    private List<ItemDataSO.ItemData> DouguItemDatasList = new List<ItemDataSO.ItemData>();
+    [SerializeField]
+    private List<ItemDataSO.ItemData> DaijinamonoItemDatasList = new List<ItemDataSO.ItemData>();
+
+    // ItemType ごとに分類したアイテムの名前の List
+    [SerializeField]
+    private List<string> BukiItemNamesList = new List<string>();　//　<=　ItemType が Equip である文字列だけを管理する List 
+    [SerializeField]
+    private List<string> BouguItemNamesList = new List<string>();
+    [SerializeField]
+    private List<string> DouguItemNamesList = new List<string>();
+    [SerializeField]
+    private List<string> DaijinamonoItemNamesList = new List<string>();
+
 
     private void Awake()
     {
@@ -24,6 +50,12 @@ public class DataBaseManager : MonoBehaviour
             //その DataBaseManager クラスのゲームオブジェクトを破壊する。
             Destroy(gameObject);
         }
+        // アイテムの種類別の List を作成
+        CreateItemTypeList();
+
+        // アイテムの名前をアイテムの種類ごとに分類して List を作成
+        CreateItemNamesListFromItemData();
+
     }
 
     /// <summary>
@@ -48,4 +80,81 @@ public class DataBaseManager : MonoBehaviour
         // 上記の処理の結果、該当する EvenData の情報がスクリプタブル・オブジェクト内にない場合には、null を戻す
         return null;
     }
+
+    /// <summary>
+    /// アイテムの種類別の List を作成
+    /// </summary>
+    private void CreateItemTypeList()
+    {
+
+        // ItemDataSO スクリプタブル・オブジェクト内の ItemData の情報を１つずつ順番に取り出して itemData 変数に代入
+        foreach (ItemDataSO.ItemData itemData in itemDataSO.itemDataList)
+        {
+
+            // 現在取り出している itemData 変数の ItemType の値がどの case と合致するかを判定
+            switch (itemData.itemType)
+            {
+
+                // itemData.itemType == ItemType.Equip の場合には、equipItemDatasList 変数に itemData 変数の値を追加する
+                case ItemType.Buki:
+                    BukiItemDatasList.Add(itemData);
+                    break;
+
+                case ItemType.Bougu:
+                    BouguItemDatasList.Add(itemData);
+                    break;
+
+                case ItemType.Dougu:
+                    DouguItemDatasList.Add(itemData);
+                    break;
+
+                case ItemType.Daijinamono:
+                    DaijinamonoItemDatasList.Add(itemData);
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// アイテムの名前をアイテムの種類ごとに分類して List を作成
+    /// </summary>
+    private void CreateItemNamesListFromItemData()
+    {
+
+        // ItemName 型の enum に登録されている列挙子をすべて取り出して文字列の配列にして取得し、values 変数に代入
+        string[] values = Enum.GetNames(typeof(ItemName));
+
+        // ItemDataSO スクリプタブル・オブジェクト内の ItemData の情報を１つずつ順番に取り出して itemData 変数に代入
+        foreach (ItemDataSO.ItemData itemData in itemDataSO.itemDataList)
+        {
+
+            // values 配列変数の中を先頭から順番に検索し、現在取り出している itemData 変数の itemName と合致した値があれば 
+            if (!string.IsNullOrEmpty(values.FirstOrDefault(x => x == itemData.itemName.ToString())))
+            {
+
+                // 最初に合致した値を文字列として代入
+                string itemName = itemData.itemName.ToString();
+
+                // 現在取り出している itemData 変数の ItemType の値がどの case と合致するかを判定(ここは上のメソッドの switch 文と同じ分岐にしても実装できます。学習のためにキャスト処理を入れています)
+                switch ((int)itemData.itemType)
+                {
+
+                    // itemData.itemType == 0(ItemType 型の列挙子の最初のもの)
+                    case 0:
+                        BukiItemNamesList.Add(itemName);
+                        break;
+                    case 1:
+                        BouguItemNamesList.Add(itemName);
+                        break;
+                    case 2:
+                        DouguItemNamesList.Add(itemName);
+                        break;
+                    case 3:
+                        DaijinamonoItemNamesList.Add(itemName);
+                        break;
+                }
+            }
+        }
+    }
+
 }
--- Assets/Scripts/ItemType.cs
+++ Assets/Scripts/ItemType.cs
@@ -3,10 +3,10 @@
 /// </summary>
 public enum ItemType
 {
-    武器,         // 装備品
-    防具,         // 売却専用品
-    道具,         // 貴重品
-    大事なもの     // 使用品
+    Buki,         // 装備品
+    Bougu,         // 売却専用品
+    Dougu,         // 貴重品
+    Daijinamono     // 使用品
 
     // TODO　種類が増える場合には追記する
 
